94e10aa1a799c57fb1ac037c226b60d8b13df6d4
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2024-08-24 12:34:44 +0630

    Add flaticon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,17 +12,6 @@
         {{-- Swiper CSS1 JS1 --}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
-        {{-- Flat Icon --}}
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-regular-straight/css/uicons-regular-straight.css" />
-        <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.5.1/uicons-solid-rounded/css/uicons-solid-rounded.css'>
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-bold-straight/css/uicons-bold-straight.css" />
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-thin-rounded/css/uicons-thin-rounded.css" />
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-bold-rounded/css/uicons-bold-rounded.css" />
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-regular-rounded/css/uicons-regular-rounded.css" />
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-thin-straight/css/uicons-thin-straight.css" />
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-solid-straight/css/uicons-solid-straight.css" />
-        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.5.1/uicons-brands/css/uicons-brands.css" />
-
         @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
     <body class="">
